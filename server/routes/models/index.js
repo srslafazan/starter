54ea6c8f0816bcc9db617ceb276db0caf1aa6d06
Shain <srslafazan@gmail.com>
2018-08-25 17:25:20 -0700

    Minor updates

--- server/routes/models/index.js
+++ server/routes/models/index.js
@@ -1,5 +1,8 @@
 const models = require('express').Router();
 
-models.get('/', (req, res) => {console.log('/models');res.status(200).json({connected: true})});
+models.get('/', (req, res) => {
+  console.log('/models -- TODO');
+  res.status(200).json({connected: true});
+});
 
 module.exports = models;
