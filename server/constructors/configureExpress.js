0fed3af6861a686387ea870abee494e89fb60b47
Shain <srslafazan@gmail.com>
2018-08-25 17:23:41 -0700

    Move to extended body parser

--- server/constructors/configureExpress.js
+++ server/constructors/configureExpress.js
@@ -24,7 +24,7 @@ const configureExpress = (app) => {
   app.use(express.static(path.resolve(__dirname, '../../client/dist')))
 
   app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.urlencoded({ extended: true }))
   app.use(cookieParser())
   app.use(cors());
   app.use(expressSession({
